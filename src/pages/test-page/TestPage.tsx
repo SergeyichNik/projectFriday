3d20206d06bdba66b4330eb796c0ef28b8d05494
SergeichNik <tyunev.nik89@gmail.com>
2022-05-10 11:18:32 +0300

    super components add

--- src/pages/test-page/TestPage.tsx
+++ src/pages/test-page/TestPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import SuperInputText from "../../components/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../components/common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../../components/common/c3-SuperCheckbox/SuperCheckbox";
 
 const TestPage = () => {
     return (
         <div>
             Test page
+            <div>
+                <SuperInputText/>
+            </div>
+            <div>
+                <SuperCheckbox/>
+                <SuperButton/>
+            </div>
         </div>
     );
 };
