de7b330e21b58ea92fc47c7a09a67ea78c91083f
Ilya030290 <anoshko90@mail.ru>
2022-05-29 11:19:10 +0300

    Add dependencies MUI, formik, yup

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -3,8 +3,7 @@ import classes from "./CommonStyle.module.css";
 const Registration = () => {
     return (
         <div className={classes.common}>
-             <h1>REGISTRATION</h1>
-            <p>Hello Registration</p>
+
         </div>
     );
 };
