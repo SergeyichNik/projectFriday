0977e4bbb90532b879995c77625895610ec4f3c5
chrismelyan <chris.shelpuk@gmail.com>
2022-05-29 20:34:32 +0200

    login: installed libraries

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,15 +4,6 @@ const Login = () => {
     return (
         <div className={classes.common}>
             <h1>LOGIN</h1>
-            <h2>Hello world</h2>
-            <p>Christina</p>
-
-
-
-
-
-
-            <h1> Get started </h1>
         </div>
     );
 };
