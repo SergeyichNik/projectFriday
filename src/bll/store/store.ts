be7ecf9ca876eae4cc6453b2dcb3111b6a355992
SergeichNik <tyunev.nik89@gmail.com>
2022-05-29 11:29:21 +0300

    add applyMiddleware() & thunk to store.ts

--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,13 +1,14 @@
-import {combineReducers} from "redux";
+import {applyMiddleware, combineReducers} from "redux";
 import {legacy_createStore as createStore} from "redux";
 import {AppReducer} from "../reducers/app-reducer";
+import thunk from "redux-thunk";
 
 
 const reducer = combineReducers({
     appReducer: AppReducer
 })
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof reducer>
 
