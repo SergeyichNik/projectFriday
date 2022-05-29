c758a8b1c8c41ec183140aff85744a30c4e9a917
Ilya030290 <anoshko90@mail.ru>
2022-05-29 12:52:38 +0300

    changes for registration-reducer

--- src/bll/reducers/registration-reducer.ts
+++ src/bll/reducers/registration-reducer.ts
@@ -1,14 +1,34 @@
+import {authAPI, RegistrationParamsType} from "../../api/api";
+import {ThunkType} from "../store/store";
 
-type InitialStateType = {}
-type RegistrationActionsType = ReturnType<typeof registrationAC>;
+const InitialState = {
+    isRegistered: false
+}
 
-export const RegistrationReducer = (state: InitialStateType = {}, action: RegistrationActionsType ) => {
+//Types
+type InitialStateType = typeof InitialState;
+export type RegistrationActionType = ReturnType<typeof setRegistrationAC>;
+
+//Reducer
+export const RegistrationReducer = (state: InitialStateType, action: RegistrationActionType) => {
     switch (action.type) {
         case "REGISTRATION":
-            return state
+            return {...state, isRegistered: action.isRegistered};
         default:
             return state;
     }
 }
+//ActionCreator
+export const setRegistrationAC = (isRegistered: boolean) => ({type: "REGISTRATION", isRegistered} as const);
 
-const registrationAC = () => ({type: "REGISTRATION"} as const);
+//ThunkCreator
+export const registrationTC = (data: RegistrationParamsType): ThunkType => (dispatch) => {
+    authAPI.registration(data)
+        .then((res) => {
+            console.log(res.data);
+            dispatch(setRegistrationAC(true));
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+}
--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,8 +1,8 @@
 import {applyMiddleware, combineReducers} from "redux";
 import {legacy_createStore as createStore} from "redux";
 import {AppReducer} from "../reducers/app-reducer";
-import thunk from "redux-thunk";
-import {RegistrationReducer} from "../reducers/registration-reducer";
+import thunk, {ThunkAction} from "redux-thunk";
+import {RegistrationActionType, RegistrationReducer} from "../reducers/registration-reducer";
 
 
 const reducer = combineReducers({
@@ -13,5 +13,8 @@ const reducer = combineReducers({
 const store = createStore(reducer, applyMiddleware(thunk))
 
 export type AppRootStateType = ReturnType<typeof reducer>
+export type AppRootActionsType = RegistrationActionType
+
+export type ThunkType<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, AppRootActionsType>
 
 export default store
