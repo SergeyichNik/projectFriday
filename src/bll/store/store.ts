aa22c235db4b408d04c8267d9ce28be0090b8019
SergeichNik <tyunev.nik89@gmail.com>
2022-05-31 19:24:20 +0300

    merge commit

--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -2,14 +2,12 @@ import {applyMiddleware, combineReducers} from "redux";
 import {legacy_createStore as createStore} from "redux";
 import {AppReducer} from "../reducers/app-reducer";
 import thunk from "redux-thunk";
-import {newPasswordReducer} from "../reducers/newPasswordReducer";
 import {loginReducer} from "../reducers/login-reducer";
 
 
 const reducer = combineReducers({
     appReducer: AppReducer,
-    newPassword: newPasswordReducer,
-    login: loginReducer
+    login: loginReducer,
 })
 
 const store = createStore(reducer, applyMiddleware(thunk))
